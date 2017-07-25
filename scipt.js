aa8eee125afb76eb9b79109570860d4f1fd5f6b9
maya-or-psyco <maya.pleau@gmail.com>
2017-07-25 19:42:37 -0400

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,11 @@
+function function1()
+var x = document.getElementById("").value;
+document.getElementById("").innerHTML = thing1
+var x = document.getElementById("links");
+x.style.display = 'block';
+
+//http://www.stompoutbullying.org/index.php/information-and-resources/
+//https://www.stopbullying.gov/get-help-now/index.html
+//http://www.antibullyingpro.com/blog/being-bullied-advice
+//https://teenlineonline.org/yyp/121-help-me/
+//https://bullying.supportgroups.com/
